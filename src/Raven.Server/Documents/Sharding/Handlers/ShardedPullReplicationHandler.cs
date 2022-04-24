f69b97c7a6947691b8b052d87c86d44f583b6489
shiranshalom <shiran@ravendb.net>
2022-04-24 16:45:56 +0300

    RavenDB-18230 - PUT /databases/*/admin/tasks/pull-replication/hub

---
+++ src/Raven.Server/Documents/Sharding/Handlers/ShardedPullReplicationHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Server.Documents.Sharding.Handlers.Processors.Replication;
+using Raven.Server.Routing;
+
+namespace Raven.Server.Documents.Sharding.Handlers
+{
+    public class ShardedPullReplicationHandler : ShardedDatabaseRequestHandler
+    {
+        [RavenShardedAction("/databases/*/admin/tasks/pull-replication/hub", "PUT")]
+        public async Task DefineHub()
+        {
+            using (var processor = new ShardedPullReplicationHandlerProcessorForDefineHub(this))
+                await processor.ExecuteAsync();
+        }
+    }
+}
