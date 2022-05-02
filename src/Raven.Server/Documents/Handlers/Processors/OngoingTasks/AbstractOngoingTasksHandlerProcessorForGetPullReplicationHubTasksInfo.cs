5d80b9fa332a4a099229d12aa6d00e95895aa49c
shiranshalom <shiran@ravendb.net>
2022-05-02 11:57:39 +0300

    RavenDB-18230 - fix error

--- src/Raven.Server/Documents/Handlers/Processors/OngoingTasks/AbstractOngoingTasksHandlerProcessorForGetPullReplicationHubTasksInfo.cs
+++ src/Raven.Server/Documents/Handlers/Processors/OngoingTasks/AbstractOngoingTasksHandlerProcessorForGetPullReplicationHubTasksInfo.cs
@@ -1,7 +1,10 @@
-﻿using JetBrains.Annotations;
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Raven.Client.Documents.Operations.OngoingTasks;
 using Raven.Client.Documents.Operations.Replication;
+using Raven.Client.Exceptions;
 using Raven.Client.Http;
+using Raven.Client.Util;
 using Sparrow.Json;
 
 namespace Raven.Server.Documents.Handlers.Processors.OngoingTasks
@@ -18,6 +21,14 @@ namespace Raven.Server.Documents.Handlers.Processors.OngoingTasks
         {
             var key = RequestHandler.GetLongQueryString("key");
             return new GetPullReplicationTasksInfoOperation.GetPullReplicationTasksInfoCommand(key, nodeTag);
+        }
+
+        public override ValueTask ExecuteAsync()
+        {
+            if (ResourceNameValidator.IsValidResourceName(RequestHandler.DatabaseName, RequestHandler.ServerStore.Configuration.Core.DataDirectory.FullPath, out string errorMessage) == false)
+                throw new BadRequestException(errorMessage);
+
+            return base.ExecuteAsync();
         }
     }
 }
