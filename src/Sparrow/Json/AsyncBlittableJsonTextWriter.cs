162b6b9689abfdbe79afbb5bc92fb38d59526b98
shiranshalom <shiran@ravendb.net>
2023-03-19 16:52:05 +0200

    RavenDB-20066 - wip

--- src/Sparrow/Json/AsyncBlittableJsonTextWriter.cs
+++ src/Sparrow/Json/AsyncBlittableJsonTextWriter.cs
@@ -3,18 +3,24 @@ using System.IO;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
+using Sparrow.Binary;
 
 namespace Sparrow.Json
 {
     public class AsyncBlittableJsonTextWriter : AbstractBlittableJsonTextWriter, IAsyncDisposable
     {
+        private const int MaxBufferedSize = 1024*256;
         private readonly Stream _outputStream;
         private readonly CancellationToken _cancellationToken;
+        private MemoryStream _doubleBuffer;
+        private Task _previous;
 
         public AsyncBlittableJsonTextWriter(JsonOperationContext context, Stream stream, CancellationToken cancellationToken = default) : base(context, context.CheckoutMemoryStream())
         {
             _outputStream = stream ?? throw new ArgumentNullException(nameof(stream));
             _cancellationToken = cancellationToken;
+            context.CheckoutMemoryStream(out _doubleBuffer);
+            _previous = Task.CompletedTask;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -23,7 +29,7 @@ namespace Sparrow.Json
             var innerStream = _stream as MemoryStream;
             if (innerStream == null)
                 ThrowInvalidTypeException(_stream?.GetType());
-            if (innerStream.Length * 2 <= innerStream.Capacity)
+            if (innerStream.Length * 2 <= innerStream.Capacity && innerStream.Length < MaxBufferedSize) 
                 return new ValueTask<int>(0);
 
             FlushInternal();
@@ -32,6 +38,7 @@ namespace Sparrow.Json
 
         public async Task<int> OuterFlushAsync()
         {
+            await _previous.ConfigureAwait(false);
             var innerStream = _stream as MemoryStream;
             if (innerStream == null)
                 ThrowInvalidTypeException(_stream?.GetType());
@@ -66,7 +73,7 @@ namespace Sparrow.Json
             var innerStream = _stream as MemoryStream;
             if (innerStream == null)
                 ThrowInvalidTypeException(_stream?.GetType());
-            if (innerStream.Length * 2 <= innerStream.Capacity)
+            if (innerStream.Length * 2 <= innerStream.Capacity && innerStream.Length < MaxBufferedSize)
                 return new ValueTask<int>(0);
 
             FlushInternal(); // this is OK, because inner stream is a MemoryStream
@@ -75,17 +82,24 @@ namespace Sparrow.Json
 
         public async ValueTask<int> FlushAsync(CancellationToken token = default)
         {
+            await _previous.ConfigureAwait(false);
+         
             var innerStream = _stream as MemoryStream;
             if (innerStream == null)
                 ThrowInvalidTypeException(_stream?.GetType());
             FlushInternal();
-            innerStream.TryGetBuffer(out var bytes);
-            var bytesCount = bytes.Count;
+            var bytesCount = innerStream.Length;
             if (bytesCount == 0)
                 return 0;
-            await _outputStream.WriteAsync(bytes.Array, bytes.Offset, bytesCount, token).ConfigureAwait(false);
+
+            _doubleBuffer.SetLength(bytesCount);
+            _doubleBuffer.TryGetBuffer(out ArraySegment<byte> buffer);
+            innerStream.TryGetBuffer(out ArraySegment<byte> bytes);
+            Array.Copy(bytes.Array, bytes.Offset, buffer.Array, buffer.Offset, buffer.Count);
             innerStream.SetLength(0);
-            return bytesCount;
+            
+            _previous = _outputStream.WriteAsync(buffer.Array, buffer.Offset, buffer.Count, token);
+            return buffer.Count;
         }
 
         public async ValueTask DisposeAsync()
@@ -93,7 +107,10 @@ namespace Sparrow.Json
             DisposeInternal();
 
             if (await FlushAsync().ConfigureAwait(false) > 0)
+            {
+                await _previous.ConfigureAwait(false);
                 await _outputStream.FlushAsync().ConfigureAwait(false);
+            }
 
             _context.ReturnMemoryStream((MemoryStream)_stream);
         }
