a5994a67b54d02747bad65db67c29a17197b9ebd
Marcin Lewandowski <marcin@ravendb.net>
2024-10-22 09:24:30 +0200

    RavenDB-22955 Replication - Usability: Add Replication Progress

--- tools/TypingsGenerator/Program.cs
+++ tools/TypingsGenerator/Program.cs
@@ -61,6 +61,7 @@ using Raven.Server.Dashboard;
 using Raven.Server.Dashboard.Cluster;
 using Raven.Server.Dashboard.Cluster.Notifications;
 using Raven.Server.Documents;
+using Raven.Server.Documents.Commands.Replication;
 using Raven.Server.Documents.ETL;
 using Raven.Server.Documents.ETL.Providers.ElasticSearch.Test;
 using Raven.Server.Documents.ETL.Providers.OLAP;
@@ -516,6 +517,10 @@ namespace TypingsGenerator
             scripter.AddType(typeof(SubscriptionOpeningStrategy));
             scripter.AddType(typeof(SubscriptionTaskPerformanceStats));
 
+            // ongoing tasks - non-etl progress
+            scripter.AddType(typeof(GetReplicationOngoingTasksProgressCommand.ReplicationTaskProgressResponse));
+            scripter.AddType(typeof(GetOutgoingInternalReplicationProgressCommand.ReplicationTaskProgressResponse));
+            
             // ongoing tasks - ravenDB ETL
             scripter.AddType(typeof(EtlTaskProgress));
             scripter.AddType(typeof(OngoingTaskRavenEtl));
