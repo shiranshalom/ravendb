0c1c7468d13a92304cc4f04083a1ec3963066803
Damian Olszewski <damian@ravendb.net>
2024-12-16 13:09:58 +0100

    RavenDB-23362 Add isRoundedPill to Select

--- src/Raven.Studio/typescript/components/common/select/SelectCreatable.tsx
+++ src/Raven.Studio/typescript/components/common/select/SelectCreatable.tsx
@@ -1,19 +1,34 @@
-import React, { ComponentProps } from "react";
+import { ComponentProps } from "react";
 import ReactSelectCreatable from "react-select/creatable";
 import { GroupBase } from "react-select";
 import "./Select.scss";
+import classNames from "classnames";
+import { applyRoundedPillStyle } from "components/common/select/Select";
+
+interface SelectCreatableProps<
+    Option,
+    IsMulti extends boolean = false,
+    Group extends GroupBase<Option> = GroupBase<Option>,
+> extends ComponentProps<typeof ReactSelectCreatable<Option, IsMulti, Group>> {
+    isRoundedPill?: boolean;
+}
 
 export default function SelectCreatable<
     Option,
     IsMulti extends boolean = false,
     Group extends GroupBase<Option> = GroupBase<Option>,
->(props: ComponentProps<typeof ReactSelectCreatable<Option, IsMulti, Group>>) {
+>({ isRoundedPill, className, styles = {}, ...rest }: SelectCreatableProps<Option, IsMulti, Group>) {
+    if (isRoundedPill) {
+        applyRoundedPillStyle(styles);
+    }
+
     return (
         <ReactSelectCreatable
-            {...props}
-            className="bs5 react-select-container"
-            classNamePrefix="react-select"
+            styles={styles}
             formatCreateLabel={(value) => value}
+            {...rest}
+            classNamePrefix="react-select"
+            className={classNames("bs5 react-select-container", { "rounded-pill": isRoundedPill }, className)}
         />
     );
 }
